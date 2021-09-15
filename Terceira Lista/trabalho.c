77eaae6be49db25ba60136d74b43421c011e39d2
Guilherme Lima <guilhermedallmann12@globomail.com>
2021-09-15 14:09:11 -0300

    Quase lá

--- Terceira Lista/trabalho.c
+++ Terceira Lista/trabalho.c
@@ -12,11 +12,12 @@
 #define PROX (sizeof(void **))
 #define ANTERIOR (sizeof(void **))
 
-void addPerson(void *pBuffer, void *linkedlist,  void *pFirst);
-
+void *addPerson(void *pBuffer, void *linkedlist,  void *pFirst);
+//void show(void *pBuffer, void *linkedlist, void *pFirst, void *pRun);
 int main(){
     void *linkedlist;
     void *pFirst;
+    void *pRun;
     void *pBuffer = malloc(OPTION + CONTADOR);
      if(!pBuffer){
                 printf("Erro de memória!");
@@ -34,8 +35,9 @@ int main(){
         {
         case 1:
             *(int *)(pBuffer + OPTION) = *(int *)(pBuffer + OPTION) + 1;
-           addPerson(pBuffer, linkedlist, pFirst);
-        
+             pFirst = addPerson(pBuffer, linkedlist, pFirst);
+            printf("%s", pFirst);
+            printf("\n");
             break;
         default:
             printf("Opção Invalida!\n");
@@ -45,16 +47,20 @@ int main(){
 
 }
 
-void addPerson(void *pBuffer, void *linkedlist, void *pFirst){
+void *addPerson(void *pBuffer, void *linkedlist, void *pFirst){
     linkedlist = malloc(NAME + AGE + NUMBER + ANTERIOR + PROX); 
     *(void **)(linkedlist + NAME + AGE + NUMBER ) = NULL;
     *(void **)(linkedlist + NAME + AGE + NUMBER + ANTERIOR ) = NULL;
     void *aux;
-    if(*(int *)(pBuffer + OPTION) == 1){
+
+     if(*(int *)(pBuffer + OPTION) == 1){
         pFirst = linkedlist;
         
     }
+   if(*(int *)(pBuffer + OPTION) != 1){
 
+       *(int *)((pFirst + (NAME + AGE + NUMBER + ANTERIOR) * (*(int *)(pBuffer + OPTION)))) = linkedlist;
+   }
     printf("Digite o nome: ");
     scanf("%s", (char *)(linkedlist)); // Adicionando nome;
     printf("Digite a idade: ");
@@ -62,20 +68,27 @@ void addPerson(void *pBuffer, void *linkedlist, void *pFirst){
     printf("Digite o numero: ");
     scanf("%d", (int *)(linkedlist + NAME + AGE)); // Adicionando numero;
     aux = pFirst;  // Aux para percorrer o vetor;
-    printf("%p", *(void **)(linkedlist + NAME + AGE + NUMBER + ANTERIOR )); // valor de memória proximo.
     printf("\n");
-    printf("%s\n", linkedlist);
-    printf("%d\n", *(int *)(linkedlist + NAME));
-    printf("%d\n", *(int *)(linkedlist + NAME + AGE));
+    printf("%p\n", *(void **)((pFirst + (NAME + AGE + NUMBER + ANTERIOR) * (*(int *)(pBuffer + OPTION))))); // Valor do proximo
+    printf("%p\n", linkedlist); // Valor de memória onde começa o proximo
+    printf("%s", pFirst);
     printf("\n");
-    printf("%p\n", linkedlist);
-    do{ // Percorrer toda lista encadeada
+
+    /*do{ // Percorrer toda lista encadeada
                 aux = *(void **)(linkedlist + NAME + AGE + NUMBER + ANTERIOR );
                 printf("%p\n", aux); // Printa o proximo espaço de memória
                 getchar();
-    }while(*(void **)(linkedlist + NAME + AGE + NUMBER + ANTERIOR ) != NULL);
-    
+    }while(*(void **)(linkedlist + NAME + AGE + NUMBER + ANTERIOR ) != NULL);*/
+    return pFirst;
 
 
 
 }
+/*
+pFirst * 2 // 2 pessoa
+pFirst * 3 // 3 pessoa*/
+/*
+void show(void *pBuffer, void *linkedlist, void *pFirst, void *pRun){
+        pRun = pFirst;
+        
+}*/
