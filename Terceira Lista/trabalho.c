2a33192ba24b77a60857a3865de1a93952d352fd
Guilherme Lima <guilhermedallmann12@globomail.com>
2021-09-16 20:56:23 -0300

    Removed, Procurar e printar Ok, falta o short do ADD

--- Terceira Lista/trabalho.c
+++ Terceira Lista/trabalho.c
@@ -39,7 +39,6 @@ int main(){
                 exit(0);
             }
 
-                pFirst = NULL;
     *(int *)(pBuffer + OPTION) = 0;
     do{
         printf("Digite a opção: \n");
@@ -51,12 +50,22 @@ int main(){
         {
         case 1:
             *(int *)(pBuffer + OPTION) = *(int *)(pBuffer + OPTION) + 1;
-            pFirst = addPerson(pBuffer, linkedlist, pFirst);
+            pFirst = (void *)addPerson(pBuffer, linkedlist, pFirst);
+            printf("%p\n", pFirst);
             printf("\n");
             break;
         case 2:
-            pFirst = removed(pFirst, pBuffer, pRun);
+            if(*(int *)(pBuffer + OPTION) == 0){
+                printf("Lista vazia!\n");
+                printf("\n");
+                break;
+            }
+            else{
+            pFirst = (void *)removed(pFirst, pBuffer, pRun);
+            *(int *)(pBuffer + OPTION) = *(int *)(pBuffer + OPTION) - 1;
             break;
+
+            }
         case 3:
             search(pFirst, pRun, pBuffer);
             printf("\n");
@@ -74,10 +83,6 @@ int main(){
         }
     }while(*(int *)pBuffer != 5);
     sair(pFirst, pBuffer, pRun);
-    free(pBuffer);
-    free(pRun);
-    free(pFirst);
-    free(linkedlist);
 }
 
 void *addPerson(void *pBuffer, void *linkedlist, void *pFirst){
@@ -96,9 +101,9 @@ void *addPerson(void *pBuffer, void *linkedlist, void *pFirst){
       printf("\n");
     *(void **)(linkedlist + NAME + AGE + NUMBER ) = NULL; //Zera o anterior
     *(void **)(linkedlist + NAME + AGE + NUMBER + ANTERIOR ) = NULL; //Zera o Proximo
-    printf("%s\n", (char *)linkedlist); 
+    
     if(*(int *)(pBuffer + OPTION) == 1){
-        pFirst = linkedlist;
+        *(void **)pFirst = linkedlist;
         *(void **)(pFirst + PRIMEIRO) = linkedlist;
     }
    else{  
@@ -113,29 +118,35 @@ void *addPerson(void *pBuffer, void *linkedlist, void *pFirst){
 
 }
 
-void show(void *pFirst, void *pRun, void *pBuffer){
-        pRun = pFirst;
-        *(int *)(pBuffer) = 1;
+void show(void *pFirst, void *pRun, void *pBuffer){ // Printar OK
+        pRun = *(void **)pFirst;
+      if(*(int *)(pBuffer + OPTION) == 0){
+          printf("Lista vazia!\n");
+            return;
+      }
       while(pRun != NULL){
-          printf("Nome: %s\n", (char *)pRun);
+    
+          printf("Nome: %s\n", pRun);
           printf("Idade: %d\n", *(int *)(pRun + NAME));
           printf("Numero: %d\n", *(int *)(pRun + NAME + AGE));
           printf("\n");
-          *(int *)(pBuffer) += 1;
             pRun = *(void **)(pRun + NAME + AGE + NUMBER + ANTERIOR);
-            printf("%p\n", pRun);
       }    
       return;
 }
 
-void search(void *pFirst, void *pRun, void *pBuffer){
+void search(void *pFirst, void *pRun, void *pBuffer){ // Procurar OK
 
+    if(*(int *)(pBuffer + OPTION) == 0){
+          printf("Lista vazia!\n");
+            return;
+      }
     printf("Digite o nome que queira buscar: ");
     scanf("%s", (char *)(pBuffer + OPTION + CONTADOR));
     pRun = pFirst;
     while(pRun != NULL){
         if(strcmp(pRun,(char *)(pBuffer + OPTION + CONTADOR)) == 0){
-          printf("Nome: %s\n", pRun);
+          printf("Nome: %s\n", (char *)pRun);
           printf("Idade: %d\n", *(int *)(pRun + NAME));
           printf("Telefone: %d\n", *(int *)(pRun + NAME + AGE));
           printf("%p\n", *(void **)(pRun + NAME + AGE + NUMBER + ANTERIOR));
@@ -150,14 +161,15 @@ void search(void *pFirst, void *pRun, void *pBuffer){
 void *removed(void *pFirst, void *pBuffer, void *pRun){
 
         pRun = pFirst;
-        pFirst = *(void **)(pRun + NAME + AGE + NUMBER + ANTERIOR);
+        *(void **)pFirst = *(void **)(pRun + NAME + AGE + NUMBER + ANTERIOR);
         free(pRun);
-        *(void **)(pFirst + NAME + AGE + NUMBER ) = NULL;
+        *(void **)(pFirst + NAME + AGE + NUMBER) = NULL;
+        printf("Removido com sucesso!\n");
         return pFirst;
 }
 
 void *sair(void *pFirst, void *pBuffer, void *pRun){
-    pRun = pFirst;
+    pRun = *(void **)pFirst;
     while(pRun != NULL){
         pFirst = *(void **)(pRun + NAME + AGE + NUMBER + ANTERIOR);
         free(pRun);
